7454bbdcb678d7dece1e4be47469cf173b29687e
Niteesh Kulhari <nkulhari96@gmail.com>
2024-04-18 14:49:14 -0400

    Added DB Models

---
+++ models/comments.models.js
@@ -0,0 +1,27 @@
+import mongoose, { mongo } from "mongoose";
+
+const commentSchema = mongoose.Schema({
+    projectId:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Project',
+        required: true
+    },
+    userId:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    content:{
+        type: String,
+        required: true
+    },
+    date:{
+        type: Date,
+        default: Date.now
+    }
+})
+
+
+const Comment = mongoose.model('Comment', commentSchema);
+
+export default Comment;
