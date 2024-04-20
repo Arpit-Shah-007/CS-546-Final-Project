e31d040622fcf7d48f30c39dc64f0487ccba9ffd
Niteesh Kulhari <nkulhari96@gmail.com>
2024-04-20 19:08:49 -0400

    Corrected errors

--- routes/report.routes.js
+++ routes/report.routes.js
@@ -1,5 +1,5 @@
 import express from "express";
-
+import { createReport, getAllReports, getReportById, deleteReportById } from "../controllers/report.controller.js";
 const router = express.Router();
 
 // Route to create a new report
--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 import express from "express"
 import connectToDB from "./db/connectToMongoDB.js";
-
+import cors from 'cors';
+import mainRouter from "./routes/index.js";
 const app = express();
 
+app.use(cors());
+app.use(express.json());
 
 
 
+app.use("/api/v1/", mainRouter);
 
 
 
